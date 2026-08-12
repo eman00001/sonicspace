53ee37770945bf7ec18790cc353765e562e032e1
eman00001 <emiyusuffe@gmail.com>
2026-08-12 01:44:29 -0300

    feat/refactor(frontend): renamed record components, implemented records displaying on shelves

--- renderer/src/Scene.tsx
+++ renderer/src/Scene.tsx
@@ -2,7 +2,7 @@ import { Canvas, useThree} from "@react-three/fiber";
 import './Scene.css';
 import { Store_1 } from "./components/environments/Stores";
 import { CONSTANTS } from "./constants/constants";
-import { RecordCover, Record } from "./components/objects/Records";
+import { RecordSquare, RecordDisc, RecordLayer } from "./components/objects/Records";
 import { OrbitControls } from "@react-three/drei";
 import { useEffect, useMemo } from "react";
 import * as THREE from "three";
@@ -24,14 +24,14 @@ function WallsAndFloor() {
 
   if (size_x < CONSTANTS.WINDOW_BREAKPOINT) {
     return (
-      <mesh material={wfmaterials} position={[0, CONSTANTS.ROOM_Y_OFFSET, CONSTANTS.BACK_WALL_Z_OFFSET + size_z / 2]}>
+      <mesh material={wfmaterials} position={[0, CONSTANTS.ROOM_Y_OFFSET, CONSTANTS.BACK_WALL_Z_OFFSET + size_z / 2]} receiveShadow>
         <boxGeometry args={[CONSTANTS.WINDOW_BREAKPOINT, size_y, CONSTANTS.WINDOW_BREAKPOINT + 0.2]} />
       </mesh>
     );
   }
 
   return (
-      <mesh material={wfmaterials} position={[0, CONSTANTS.ROOM_Y_OFFSET, CONSTANTS.BACK_WALL_Z_OFFSET + size_z / 2]}>
+      <mesh material={wfmaterials} position={[0, CONSTANTS.ROOM_Y_OFFSET, CONSTANTS.BACK_WALL_Z_OFFSET + size_z / 2]} receiveShadow>
         <boxGeometry args={[size_x, size_y, size_z]} />
       </mesh>
   );
@@ -66,6 +66,35 @@ function CameraHelper() {
 }
 
 function Scene() {
+  // hard code for now but can be dynamic later if shelf size is dynamic
+  const recordSquareCount = 8;
+  const recordSquareRowCount = 8;
+  const recordSquareColumnCount = 2;
+  const recordSquareLayerCount = 2;
+  const squareSize: [number, number, number] = [1.2, 1.2, 0.05];
+
+  // provide one image path per cover (served from public/)
+  const coverPaths = Array.from({ length: recordSquareCount }, (_, i) => `https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/03/7f/ef/037fef16-46d5-f338-62e2-e974342d5be2/artwork.jpg/1000x1000bb.jpg`);
+  // const textures = useTexture(coverPaths);
+
+  const recordLayers = Array.from({ length: recordSquareLayerCount }, (_, index) => (
+    <RecordLayer
+      key={index}
+      position={[-1.5, CONSTANTS.FLOOR_SHELF_UPPER_BOTTOM, -3.3 + (0.15 * index)]}
+      rotation={[-Math.PI / 12, 0, 0]}
+      squareSize={squareSize}
+      texturePath ={coverPaths[index]}
+    />
+  ));
+  const recordSquares = Array.from({ length: recordSquareCount }, (_, index) => (
+    <RecordSquare
+      key={index}
+      position={[-1.5, CONSTANTS.FLOOR_SHELF_UPPER_BOTTOM, -3.3 + (0.15 * index)]}
+      rotation={[-Math.PI / 12, 0, 0]}
+      squareSize={squareSize}
+      texturePath ={coverPaths[index]}
+    />
+  ));
 
   
 
@@ -76,8 +105,9 @@ function Scene() {
       <CameraHelper />
       <Store_1 position={[0, -3, -1]} />
 
-      <RecordCover></RecordCover>
-      <Record position={[0, 3, -1]}></Record>
+      {recordSquares}
+      {/* <RecordSquare position={[-1.5, -1, -3]}></RecordSquare> */}
+      <RecordDisc position={[-1.6, 1.75, -3.5]} rotation={[-Math.PI / 12, 0, 0]}></RecordDisc>
       <WallsAndFloor></WallsAndFloor>
       <OrbitControls  
         target={[0, 0, 0]}
--- renderer/src/components/objects/Records.tsx
+++ renderer/src/components/objects/Records.tsx
@@ -1,44 +1,100 @@
 import React, { useMemo, useEffect } from 'react';
 import * as THREE from 'three';
+import { useTexture } from '@react-three/drei';
 // Use a permissive props shape to avoid relying on r3f internal typings
 type MeshProps = Record<string, any>;
 
 type Vec3 = [number, number, number];
 
-interface Props extends MeshProps {
+interface RecordSquareProps extends MeshProps {
   position?: Vec3;
   rotation?: Vec3;
   coverColor?: string;
   recordColor?: string;
-  coverSize?: [number, number, number];
+  squareSize?: [number, number, number];
+  texturePath?: string;
   recordRadius?: number;
   recordThickness?: number;
 }
 
-export const RecordCover: React.FC<Props> = ({
+export const RecordSquare: React.FC<RecordSquareProps> = ({
   position = [0, 0, 0],
   rotation = [0, 0, 0],
   coverColor = '#111',
-  coverSize = [1.4, 1.4, 0.05],
+  squareSize = [1.2, 1.2, 0.05],
+  texturePath = '',
   ...props
 }) => {
-  const geom = useMemo(() => new THREE.BoxGeometry(coverSize[0], coverSize[1], coverSize[2]), [coverSize]);
-  const mat = useMemo(() => new THREE.MeshStandardMaterial({ color: coverColor, metalness: 0.1, roughness: 0.6 }), [coverColor]);
+  const coverTexture = useTexture(texturePath);
 
-  useEffect(() => {
-    return () => {
-      geom.dispose();
-      // @ts-ignore
-      mat.dispose && mat.dispose();
-    };
-  }, [geom, mat]);
+  const geom = useMemo(
+    () => new THREE.BoxGeometry(squareSize[0], squareSize[1], squareSize[2]),
+    [squareSize]
+  );
 
   return (
-    <mesh geometry={geom} material={mat} position={position} rotation={rotation} {...props} />
+    <mesh geometry={geom} position={position} rotation={rotation} {...props}>
+      <meshStandardMaterial map={coverTexture} />
+    </mesh>
   );
 };
 
-export const Record: React.FC<Props> = ({
+interface RecordSetProps {
+  count: number;
+  bottom: number;
+  left: number;
+  back: number;
+  coverPaths: string[];
+  squareSize?: [number, number, number];
+}
+
+export const RecordSet: React.FC<RecordSetProps> = ({
+  count,
+  coverPaths,
+  squareSize = [1.2, 1.2, 0.05],
+  bottom,
+  left,
+  back,
+}) => {
+  return (
+    <>
+      {Array.from({ length: count }, (_, index) => (
+        <RecordSquare
+          key={index}
+          position={[left, bottom, back + (0.1 * index)]}
+          rotation={[-Math.PI / 12, 0, 0]}
+          squareSize={squareSize}
+          texturePath={coverPaths[index]}
+        />
+      ))}
+    </>
+  );
+};
+
+interface RecordLayerProps {
+  recordColumns: RecordSetProps[];
+  position?: [number, number, number];
+}
+
+export const RecordLayer: React.FC<RecordLayerProps> = ({
+  recordColumns,
+  position = [0, 0, 0],
+}) => {
+  return (
+    <>
+      {recordColumns.map((recordSet, index) => (
+        <group
+          key={index}
+          position={position}
+        >
+          <RecordSet {...recordSet} />
+        </group>
+      ))}
+    </>
+  );
+};
+
+export const RecordDisc: React.FC<RecordSquareProps> = ({
   position = [0, 0, 0],
   rotation = [0, 0, 0],
   recordColor = '#000',
@@ -58,9 +114,9 @@ export const Record: React.FC<Props> = ({
   }, [geom, mat]);
 
   return (
-    <mesh geometry={geom} material={mat} position={position} rotation={[Math.PI / 2, 0, 0]} {...props}>
+    <mesh geometry={geom} material={mat} position={position} rotation={[Math.PI / 2, 0, 0]} {...props} castShadow>
       {/* small center label */}
-      <mesh position={[0, 0, recordThickness / 2 + 0.001]} rotation={[0, 0, 0]}>
+      <mesh castShadow position={[0, 0, recordThickness / 2 + 0.001]} rotation={[0, 0, 0]} >
         <cylinderGeometry args={[0.08, 0.08, 0.002, 32]} />
         <meshStandardMaterial color={'#e5e5e5'} metalness={0.1} roughness={0.9} />
       </mesh>
@@ -68,4 +124,4 @@ export const Record: React.FC<Props> = ({
   );
 };
 
-export default Record;
+export default RecordDisc;
