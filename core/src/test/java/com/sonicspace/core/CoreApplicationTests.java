9246e65f122e2ed0d0f14c6c7ec9af06b8d205eb
eman00001 <emiyusuffe@gmail.com>
2026-08-13 00:07:35 -0300

    feat(backend): added backend (core)

---
+++ core/src/test/java/com/sonicspace/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonicspace.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
